4ddc988bd132e6aeee79d8aebd987881c215ceab
ksmolenwsei <krystian.smolen@microsoft.wsei.edu.pl>
2019-02-22 10:10:46 +0100

    dodanie styli i funkcji umożliwiającej poruszanie kulką

--- JavaScript/skrypt.js
+++ JavaScript/skrypt.js
@@ -6,3 +6,30 @@ let message = document.querySelector('.message');
 
 let maxY = field.clientHeight - ball.clientHeight
 let maxX = field.clientWidth - ball.clientHeight
+
+window.addEventListener('deviceorientation', game);
+
+
+function game(e) {
+  let x = e.beta;
+  let y = e.gamma;
+
+  if (x >  90) x =  90
+  if (x < -90) x = -90
+
+  x += 90;
+  y += 90;
+
+
+  ball.style.top  = (maxX*x/180 - 10) + "px";
+  ball.style.left = (maxY*y/180 - 10) + "px";
+
+  let ball_pos = {
+    top: ball.offsetTop,
+    left: ball.offsetLeft
+  }
+  let checkpoint_pos = {
+    top: start.offsetTop,
+    left: start.offsetLeft
+  }
+}
